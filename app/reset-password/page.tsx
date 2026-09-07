7d4ea3960d5de36c92ce9654fa70ea7ddb06c903
Sachin Kumar <sachin20july@gmail.com>
2026-09-07 08:32:27 +0530

    Added Suspense prerendering page code

--- app/reset-password/page.tsx
+++ app/reset-password/page.tsx
@@ -1,9 +1,17 @@
 "use client";
 
-import { FormEvent, useState } from "react";
+import { FormEvent, Suspense, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
 
 export default function ResetPasswordPage() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordForm />
+    </Suspense>
+  );
+}
+
+function ResetPasswordForm() {
   const searchParams = useSearchParams();
   const router = useRouter();
 
