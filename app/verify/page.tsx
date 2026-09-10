8d8330e1d891671299fed4b94f01f31c61ed52d1
Sachin Kumar <sachin20july@gmail.com>
2026-09-10 12:21:48 +0530

    Uncomment VerifyOtpPage function

--- app/verify/page.tsx
+++ app/verify/page.tsx
@@ -3,13 +3,13 @@
 // import { FormEvent, Suspense, useState } from "react";
 // import { useSearchParams, useRouter } from "next/navigation";
 
-// export default function VerifyOtpPage() {
+export default function VerifyOtpPage() {
 //   return (
 //     <Suspense fallback={<div>Loading...</div>}>
 //       <VerifyOtpForm />
 //     </Suspense>
 //   );
-// }
+}
 
 // function VerifyOtpForm() {
 //   const searchParams = useSearchParams();
